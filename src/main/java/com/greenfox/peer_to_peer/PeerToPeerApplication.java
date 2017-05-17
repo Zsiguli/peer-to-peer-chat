338c4d9091d0e617b8f04f61bcf221bdd778185d
zsiguli <markovichzsigmond@gmail.com>
2017-05-17 10:40:21 +0200

    chore(chat): init chat project

---
+++ src/main/java/com/greenfox/peer_to_peer/PeerToPeerApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.peer_to_peer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerToPeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeerToPeerApplication.class, args);
+	}
+}
