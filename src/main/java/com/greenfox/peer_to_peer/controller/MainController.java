d712610e47fddbb04924013765fa821ed2fb9bc8
zsiguli <markovichzsigmond@gmail.com>
2017-05-17 12:02:59 +0200

    feat(chat): logging 1.0

--- src/main/java/com/greenfox/peer_to_peer/controller/MainController.java
+++ src/main/java/com/greenfox/peer_to_peer/controller/MainController.java
@@ -3,11 +3,18 @@ package com.greenfox.peer_to_peer.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Date;
+
+import static java.util.logging.Level.INFO;
+
 @Controller
 public class MainController {
 
   @GetMapping("/")
   public String homePage() {
+    System.out.println(new Date() + " " +
+            INFO.getName() +
+            " Request / GET" );
     return "index";
   }
 }
