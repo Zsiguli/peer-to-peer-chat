f476e3e60e488dfc9e118def0983f80cf4f379ce
zsiguli <markovichzsigmond@gmail.com>
2017-05-18 16:24:40 +0200

    feat(user): can add user if not exist

--- src/main/java/com/greenfox/peer_to_peer/controller/MainController.java
+++ src/main/java/com/greenfox/peer_to_peer/controller/MainController.java
@@ -1,10 +1,13 @@
 package com.greenfox.peer_to_peer.controller;
 
 import com.greenfox.peer_to_peer.repository.UserRepository;
+import com.greenfox.peer_to_peer.service.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Date;
 import java.util.Map;
@@ -47,5 +50,13 @@ public class MainController {
     } else {
       return "enter";
     }
+  }
+
+  @PostMapping("/addUser")
+  public String addUser(@RequestParam("name") String name) {
+    if (userRepository.count() == 0) {
+      userRepository.save(new User(name));
+    }
+    return "redirect:/";
   }
 }
--- src/main/java/com/greenfox/peer_to_peer/service/User.java
+++ src/main/java/com/greenfox/peer_to_peer/service/User.java
@@ -1,6 +1,8 @@
 package com.greenfox.peer_to_peer.service;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -8,6 +10,8 @@ import javax.persistence.*;
 @Entity
 @Setter
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "users")
 public class User {
 
